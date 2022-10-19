35f3e5c1af3a251ea4bf1fb94ab9614c0a1d2686
Mfeliciano778 <1652@holbertonschool.com>
2022-10-19 15:02:07 -0400

    Pushing and committing the first two challenges that I completed

--- src/components/Contestant.jsx
+++ src/components/Contestant.jsx
@@ -1,3 +1,7 @@
+import { useState } from "react";
+
+useState
+
 function Contestant(props) {
   return (
     <div
--- src/components/Race.jsx
+++ src/components/Race.jsx
@@ -1,10 +1,13 @@
+import { useState } from 'react';
 import Contestant from './Contestant.jsx';
 import { contestantStyles } from '../styles';
 import { contestants } from '../assets/data';
 
-function Race() {
+function Race(props) {
+  const [everyContestant, setAllContestants] = useState(contestants)
   return (
     <div style={contestantStyles.contestantList}>
+      {props.currentTime}
       {contestants.map((contestant) => (
         <Contestant
           key={contestant.id}
